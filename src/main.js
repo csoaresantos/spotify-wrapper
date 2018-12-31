91e454a93efbcd49ac1bfd5935b1279829db4138
Charles S <charlessoaresdossantos@gmail.com>
2018-12-31 01:28:44 -0200

    it was created the smokes tests skeleton

---
+++ src/main.js
@@ -0,0 +1,19 @@
+export const search = function() {
+    return 1;
+}
+
+export const searchAlbums = function() {
+    return 1;
+}
+
+export const searchArtists = function() {
+    return 1;
+}
+
+export const searchTracks = function() {
+    return 1;
+}
+
+export const searchPlaylists = function() {
+    return 1;
+}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,11 +1,27 @@
 import { expect } from 'chai';
-import { search} from '../src/main';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
 describe('Spotify Wrapper', () => {
     describe('smoke test', () => {
-       it('should exists search method',() => {
-        expect(search).to.exist;
-       });
+        it('should exists search method',() => {
+            expect(search).to.exist;
+           });
+
+           it('should exists searchAlbums method',() => {
+            expect(searchAlbums).to.exist;
+           });
+
+           it('should exists searchArtits method',() => {
+            expect(searchArtists).to.exist;
+           });
+
+           it('should exists searchTracks method',() => {
+            expect(searchTracks).to.exist;
+           });
+
+           it('should exists search searchPlaylists',() => {
+            expect(searchPlaylists).to.exist;
+           });
     });
 
 });
